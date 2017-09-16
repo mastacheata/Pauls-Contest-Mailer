c3a92be0778727ed9d9d06af3af9183910a32c7c
Benedikt Bauer <mastacheata@gmail.com>
2017-09-16 06:11:05 +0200

    First Release version

---
+++ Pauls Contest Mailer/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pauls_Contest_Mailer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private Random rnd = new Random();
+
+        private SpreadsheetAnalysis analysis = null;
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".txt",
+                Filter = "Excel-Dateien (*.xls, *.xlsx)|*.xls;*.xlsx"
+            };
+
+            bool? result = dlg.ShowDialog();
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+
+                analysis = new SpreadsheetAnalysis();
+                analysis.ProcessWorkbook(filename);
+
+                if (!analysis.mailNotFound)
+                {
+                    btnSendMails.IsEnabled = true;
+                }
+            }
+        }
+
+        private void BtnSendMails_Click(object sender, RoutedEventArgs e)
+        {
+            int total = analysis.CountMails();
+
+            MessageBoxResult result = MessageBox.Show("Das kann dauern ;)" + Environment.NewLine + "Es werden " + total + " Emails im Intervall von " + slValue.Value + " Minuten versandt." + Environment.NewLine + "Erwartetes Ende: " + DateTime.Now.AddMinutes(total*slValue.Value) + Environment.NewLine + "Jetzt starten?", "Erwartete Dauer", MessageBoxButton.OKCancel);
+
+            if (result == MessageBoxResult.OK)
+            {
+                btnSendMails.IsEnabled = false;
+                btnBrowse.IsEnabled = false;
+
+                analysis.SendMails(mailGrid, slValue.Value);
+            }
+        }
+    }
+}
---
+++ Pauls Contest Mailer/SpreadsheetAnalysis.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Windows;
+
+namespace Pauls_Contest_Mailer
+{
+    public class SpreadsheetAnalysis
+    {
+        public bool mailNotFound = false;
+
+        private Dictionary<string, SmtpClient> mailClients = null;
+        private List<List<MailMessage>> contests = new List<List<MailMessage>>();
+
+        public void ProcessWorkbook(string filename)
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = null;
+            Workbook wkb = null;
+
+            try
+            {
+                excelApp = new Microsoft.Office.Interop.Excel.Application();
+                wkb = excelApp.Workbooks.Open(filename);
+
+                foreach (Worksheet ws in wkb.Sheets)
+                {
+                    if (ws.Name == "Emails")
+                    {
+                        Console.WriteLine("We found the emails tab");
+                        mailClients = ParseEmails(ws);
+                    }
+                    else if (ws.Name.Substring(0, 11) == "Gewinnspiel")
+                    {
+                        Console.WriteLine("This is a contest tab");
+                        List<MailMessage> contest = ParseContest(ws);
+                        if (mailNotFound)
+                        {
+                            return;
+                        }
+                        contests.Add(contest);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private Dictionary<string, SmtpClient> ParseEmails(Worksheet ws)
+        {
+            Dictionary<string, SmtpClient> mailSetup = new Dictionary<string, SmtpClient>();
+
+            // Parse WorkSheet
+            Range rows = ws.UsedRange;
+
+            object[,] valueArray = (object[,])rows.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+
+            for (int row = 2; row <= ws.UsedRange.Rows.Count; ++row)
+            {
+                SmtpClient tempClient = new SmtpClient
+                {
+                    Port = 25,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new System.Net.NetworkCredential($"{valueArray[row, 2]}", $"{valueArray[row, 3]}"),
+                    Host = $"{valueArray[row, 4]}"
+                };
+                try
+                {
+                    mailSetup.Add($"{valueArray[row, 1]}", tempClient);
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Duplicate credentials for Email " + $"{valueArray[row, 1]}");
+                }
+            }
+
+            return mailSetup;
+        }
+
+        private List<MailMessage> ParseContest(Worksheet ws)
+        {
+            List<MailMessage> contestMails = new List<MailMessage>();
+            Range rows = ws.UsedRange;
+
+            object[,] valueArray = (object[,])rows.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+
+            for (int row = 2; row <= ws.UsedRange.Rows.Count; ++row)
+            {
+                if (valueArray[row, 1] != null)
+                {
+                    MailMessage mail = new MailMessage($"{valueArray[row, 1]}", $"{valueArray[row, 2]}")
+                    {
+                        Subject = $"{valueArray[row, 3]}",
+                        Body = $"{valueArray[row, 4]}"
+                    };
+                    
+
+                    // If Sender Email doesn't have config, do not add it and complain!
+                    if (mailClients.ContainsKey($"{valueArray[row, 1]}"))
+                    {
+                        contestMails.Add(mail);
+                    }
+                    else
+                    {
+                        MessageBoxResult result = MessageBox.Show("Für die angegebene Absenderadresse existiert keine passende Konfiguration im Emails Tab: " + $"{valueArray[row, 1]}" + Environment.NewLine + "Möchtest du trotzdem weiter machen? (Überspringt diesen Eintrag)", "Fehlende Email Konfiguration", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            mailNotFound = true;
+                            return null;
+                        }
+                    }
+                }
+            }
+
+            return contestMails;
+        }
+
+        public int CountMails()
+        {
+            int total = 0;
+            foreach (List<MailMessage> contest in contests)
+            {
+                total += contest.Count;
+            }
+
+            return total;            
+        }
+
+        public void SendMails(System.Windows.Controls.DataGrid grid, double interval)
+        {
+            List<object> gridSource = null;
+            int idx = 1;
+
+            grid.ItemsSource = gridSource;
+
+            int intervaMillilSeconds = Convert.ToInt32(interval * 60 * 1000);
+
+            foreach (List<MailMessage> contestMails in contests)
+            {
+                foreach (MailMessage contestMail in contestMails)
+                {
+                    try
+                    {
+                        System.Threading.Thread.Sleep(intervaMillilSeconds);
+                        mailClients[contestMail.From.ToString()].Send(contestMail);
+                        gridSource.Add(new { Index = idx++, Absender = contestMail.From.ToString(), Empfaenger = contestMail.To.ToString(), Betreff = contestMail.Subject.ToString() });
+                    }
+                    catch (SmtpException smtpEx)
+                    {
+                        MessageBox.Show(smtpEx.Message, "Fehler beim Mailversand", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+    }
+}
